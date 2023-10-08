0a80cf350afbf7365918af7d6e0d985e9b292572
Sergeii Kalyna <setserg02@gmail.com>
2023-10-08 11:37:06 +0300

    change

--- src/Components/Header/Swiper.jsx
+++ src/Components/Header/Swiper.jsx
@@ -31,6 +31,7 @@ export default function SwiperBlock() {
                         <SwiperSlide key={item.id}>
                             <div className="slideBlock">
                                 <img
+                                    alt="photo"
                                     src={item.img}
                                     width="100%"
                                     height="100%"
