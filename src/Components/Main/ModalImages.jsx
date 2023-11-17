0b76d55570becf3e744d31441f883f28d8b55306
Sergeii Kalyna <setserg02@gmail.com>
2023-11-17 12:25:49 +0200

    fixed slider bugs

--- src/Components/Main/ModalImages.jsx
+++ src/Components/Main/ModalImages.jsx
@@ -37,6 +37,19 @@ export default function ModalImages({
         }
     }, [decrementIndex, incrementIndex, setCurrentPhoto])
 
+    const closeModal = (e) => {
+        console.log()
+        if (
+            e.target.id === "img" ||
+            e.target.classList.contains("modal_arrow")
+        ) {
+            return
+        } else {
+            setCurrentPhoto(null)
+            document.body.style.overflow = "visible"
+        }
+    }
+
     return (
         <section
             id="modal"
@@ -45,6 +58,7 @@ export default function ModalImages({
             }`}
             onClick={(e) => {
                 toggleImage(e.target.id)
+                closeModal(e)
             }}
         >
             <header className="modal_img_header">
@@ -55,11 +69,7 @@ export default function ModalImages({
                     <Button color="inherit" size="large" variant="text">
                         <ClearIcon
                             id="clear"
-                            style={{ width: "50px", height: "50px" }}
-                            onClick={() => {
-                                setCurrentPhoto(null)
-                                document.body.style.overflow = "visible"
-                            }}
+                            style={{ width: "30px", height: "30px" }}
                         />
                     </Button>
                 </article>
@@ -72,24 +82,36 @@ export default function ModalImages({
                 })}
             >
                 <div
-                    id="left"
                     className="arrow_container left"
                     onClick={(e) => decrementIndex(e)}
                 >
-                    <Button color="primary" size="medium" variant="outlined">
-                        <ArrowBackIosIcon />
+                    <Button
+                        color="primary"
+                        size="medium"
+                        variant="outlined"
+                        className="modal_arrow"
+                        id="left"
+                        sx={{ width: "30%", padding: "0", height: "80px" }}
+                    >
+                        <ArrowBackIosIcon className="modal_arrow" />
                     </Button>
                 </div>
                 <div className="modal_img">
-                    <img src={itemData[currentPhoto].img} alt="gym" />
+                    <img src={itemData[currentPhoto].img} alt="gym" id="img" />
                 </div>
                 <div
-                    id="right"
                     className="arrow_container right"
                     onClick={(e) => incrementIndex(e)}
                 >
-                    <Button color="primary" size="medium" variant="outlined">
-                        <ArrowForwardIosIcon />
+                    <Button
+                        color="primary"
+                        size="medium"
+                        variant="outlined"
+                        id="right"
+                        className="modal_arrow"
+                        sx={{ width: "30%", padding: "0" }}
+                    >
+                        <ArrowForwardIosIcon className="modal_arrow" />
                     </Button>
                 </div>
             </article>
