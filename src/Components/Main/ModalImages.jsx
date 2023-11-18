e9fab4262a8c4f341926e0983d27b06b2fa17625
Sergeii Kalyna <setserg02@gmail.com>
2023-11-18 09:12:43 +0200

    Revert "fixed slider bugs"
    This reverts commit 0b76d55570becf3e744d31441f883f28d8b55306.

--- src/Components/Main/ModalImages.jsx
+++ src/Components/Main/ModalImages.jsx
@@ -36,8 +36,18 @@ export default function ModalImages({
             window.removeEventListener("keydown", handleKeyDown)
         }
     }, [decrementIndex, incrementIndex, setCurrentPhoto])
-
-    const closeModal = (e) => {}
+    const closeModal = (e) => {
+        console.log()
+        if (
+            e.target.id === "img" ||
+            e.target.classList.contains("modal_arrow")
+        ) {
+            return
+        } else {
+            setCurrentPhoto(null)
+            document.body.style.overflow = "visible"
+        }
+    }
 
     return (
         <section
@@ -47,6 +57,7 @@ export default function ModalImages({
             }`}
             onClick={(e) => {
                 toggleImage(e.target.id)
+                closeModal(e)
             }}
         >
             <header className="modal_img_header">
@@ -78,20 +89,34 @@ export default function ModalImages({
                     className="arrow_container left"
                     onClick={(e) => decrementIndex(e)}
                 >
-                    <Button color="primary" size="medium" variant="outlined">
-                        <ArrowBackIosIcon />
+                    <Button
+                        color="primary"
+                        size="medium"
+                        variant="outlined"
+                        className="modal_arrow"
+                        id="left"
+                        sx={{ width: "30%", padding: "0", height: "80px" }}
+                    >
+                        <ArrowBackIosIcon className="modal_arrow" />
                     </Button>
                 </div>
                 <div className="modal_img">
-                    <img src={itemData[currentPhoto].img} alt="gym" />
+                    <img src={itemData[currentPhoto].img} alt="gym" id="img" />
                 </div>
                 <div
                     id="right"
                     className="arrow_container right"
                     onClick={(e) => incrementIndex(e)}
                 >
-                    <Button color="primary" size="medium" variant="outlined">
-                        <ArrowForwardIosIcon />
+                    <Button
+                        color="primary"
+                        size="medium"
+                        variant="outlined"
+                        id="right"
+                        className="modal_arrow"
+                        sx={{ width: "30%", padding: "0" }}
+                    >
+                        <ArrowForwardIosIcon className="modal_arrow" />
                     </Button>
                 </div>
             </article>
